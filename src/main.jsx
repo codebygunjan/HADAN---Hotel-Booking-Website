65dee7a675e0b351d277f4d70212b88cb1f8934f
codebygunjan <Gunjankhudaniya414@gmail.com>
2026-09-11 07:56:06 +0530

    fix router basename for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,9 @@ import { BrowserRouter } from "react-router-dom";
 import { ClerkProvider } from "@clerk/clerk-react";
 
 const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
+const basename = window.location.hostname.endsWith("github.io")
+  ? "/HADAN---Hotel-Booking-Website"
+  : "/";
 createRoot(document.getElementById("root")).render(
   <ClerkProvider publishableKey={clerkPubKey}>
     <BrowserRouter basename="/HADAN---Hotel-Booking-Website">
